e9292f94d7bf45dbc8eb61e369a243554ab9c138
João Pedro Prates <167091904+JpPrates21@users.noreply.github.com>
2025-06-11 18:32:06 -0300

    ClassesAbstratas

---
+++ Classeabstrata.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+
+
+
+class Pessoa(ABC): # Classe abstrata não possui objetos
+    def correr(self):
+        print("A pessoa esta correndo de manha")
+
+    @abstractmethod # Classe filha DEVE criar um metodo trabalhar
+    def trabalhar(self):
+        pass
+
+class Professor(Pessoa):
+    def trabalhar(self):
+        print("O Professor está dando aula")
+    
+
+
+
+
+p1 = Professor() 
+p1.correr()
+p1.trabalhar()
