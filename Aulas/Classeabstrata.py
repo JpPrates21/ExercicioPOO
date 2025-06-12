4a47f541aeeb81a513c1c13b36801d97861b82f6
João Pedro Prates <167091904+JpPrates21@users.noreply.github.com>
2025-06-12 16:17:03 -0300

    Atualização

--- Aulas/Classeabstrata.py
+++ Aulas/Classeabstrata.py
@@ -1,16 +1,11 @@
 from abc import ABC, abstractmethod
-
-
-
-
-class Pessoa(ABC): # Classe abstrata não possui objetos
+class Pessoa(ABC): 
     def correr(self):
         print("A pessoa esta correndo de manha")
 
-    @abstractmethod # Classe filha DEVE criar um metodo trabalhar
+    @abstractmethod 
     def trabalhar(self):
         pass
-
 class Professor(Pessoa):
     def trabalhar(self):
         print("O Professor está dando aula")
--- Exercicios/notas_alunos.py
+++ Exercicios/notas_alunos.py
@@ -1,6 +1,14 @@
-class Aluno:
-    def __init__(self, nome, serie, nota):
+from abc import ABC, abstractmethod
+class Pessoa(ABC):
+    def __init__(self, nome):
         self.nome = nome
+
+    @abstractmethod
+    def mostrar_dados(self):
+        pass
+class Aluno(Pessoa):
+    def __init__(self, nome, serie, nota):
+        super().__init__(nome)
         self.serie = serie
         self._nota = nota   # Atributo privado
 
@@ -15,14 +23,25 @@ class Aluno:
         else:
             print("A nota deve estar entre 0 e 10")
 
-  
+    def mostrar_dados(self):
+        print(self.nome, self.serie, self.nota)
         
-aluno1 = Aluno("Lucas", "8°", 7)
-print(aluno1.nota)
+class Professor(Pessoa):
+    def __init__(self, nome, disciplina):
+        super().__init__(nome)
 
+        self.disciplina = disciplina
+
+    def mostrar_dados(self):
+        print(f"Professor: {self.nome} : {self.disciplina}")
+
+prof1 = Professor("Rogerio", "Matematica")
+prof1.mostrar_dados()
+
+aluno1 = Aluno("Lucas", "8°", 7)
+aluno1.mostrar_dados()
 aluno1.nota = 9
 print(aluno1.nota)
-
 aluno1.nota = 12
 
 
